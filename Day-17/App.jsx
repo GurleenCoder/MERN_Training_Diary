7859c8d685cab9886cb1cf5d12572e2d2fda2eb3
Gurleen <gurleenmaan2006@gmail.com>
2026-07-26 18:57:39 +0530

    Add files via upload

---
+++ Day-17/App.jsx
@@ -0,0 +1,22 @@
+// function App() {
+//   return (
+//     <div>
+//       <h1>Welcome to React</h1>
+//       <p>This is my first React application.</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import JSXDemo from "./jsx-demo";
+
+function App() {
+  return (
+    <div>
+      <JSXDemo />
+    </div>
+  );
+}
+
+export default App;
